6175a57c5d58703c9b63bbe39a4947351eee4b57
Jamie <jamiefriel@googlemail.com>
2022-04-08 22:52:21 +0100

    fixes to token and token type

--- include/jaql/token.h
+++ include/jaql/token.h
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <any>
 #include "token_type.h"
 
 namespace Jaql{
     class Token{
         public:
-            Token(TokenType type, std::string lexeme, std::string literal, int line);
+            Token(TokenType type, std::string lexeme, std::any literal, int line);
+            Token(TokenType type, std::string lexeme, int line);
+
             std::string to_string() const;
             TokenType type;
             std::string lexeme;
-            std::string literal;
+            std::any literal;
             int line;
     };
 };
