abc4aed2fa4f2a3bba4d021796da85cbb8389578
Jamie <jamiefriel@googlemail.com>
2022-04-10 22:50:09 +0100

    initial skeleton of ast generation

---
+++ src/codegen/generate_ast.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+class ASTGen
+{
+    private:
+        /* data */
+        
+    public:
+        ASTGen(/* args */);
+        void main(std::string args){
+
+        }
+};
+
+
+
+
+int main(){
+    
+    ASTGen ast_gen;
+    ast_gen.main("hello");
+}
