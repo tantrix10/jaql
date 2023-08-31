6700ed4e33aecaed59c0fd32e4dd0d8467f731b4
Jamie Friel <jamiefriel@googlemail.com>
2023-08-31 01:42:54 +0100

    write constant and update main test

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,15 +1,11 @@
+use crate::value::{ValueArray, Value};
 
-
-#[derive(PartialEq, Eq, Debug, Clone, Copy)]
-#[repr(u8)]
-pub enum OpCode {
-    OPRETURN = 1,
-}
 
 // #[derive(Clone, Copy)]
 pub struct Chunk {
-    pub code: Vec<OpCode>,
+    pub code: Vec<u8>,
     count: i64,
+    pub constants: ValueArray
 }
 
 impl Chunk {
@@ -17,12 +13,22 @@ impl Chunk {
         Chunk {
             code: vec![],
             count: 0,
+            constants: ValueArray::new(),
         }
     }
 
-    pub fn write_chunk(&mut self, byte: OpCode){
+    pub fn write_chunk(&mut self, byte: u8){
+        // TODO: I could template this and cast opcode as u8
         self.code.push(byte);
         self.count += 1;
     }
+
+    pub fn write_constant(&mut self, constant: Value)->u8{
+        // TODO: actually check if const already exists
+        self.constants.write_value(constant);
+        return self.constants.count - 1;
+    }
+
+
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 mod chunk;
 mod disassemble;
+mod value;
+mod ops;
 
-use chunk::{Chunk, OpCode};
+use chunk::{Chunk};
 
 fn main() {
     println!("Running main.");
     let mut blob = Chunk::new();
-    blob.write_chunk(OpCode::OPRETURN);
-    blob.write_chunk(OpCode::OPRETURN);
+    let constant: u8 = blob.write_constant(42.0);
+    let constant2: u8 = blob.write_constant(43.0);
+    blob.write_chunk(ops::OP_CONSTANT as u8);
+    blob.write_chunk(constant);
+    blob.write_chunk(ops::OP_CONSTANT as u8);
+    blob.write_chunk(constant2);
+    blob.write_chunk(ops::OP_RETURN as u8);
+
     disassemble::dissasemble_chunk(&blob, "test");
 }
